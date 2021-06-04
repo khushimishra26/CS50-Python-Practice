c83eba02929221b3594580fe03f78ade1def432b
Khushi Mishra <khushimishra26peace@gmail.com>
2021-06-04 19:38:17 +0530

    Update scores.py

--- basics/scores.py
+++ basics/scores.py
@@ -3,4 +3,6 @@ scores=[]
 for i in range(3):
     scores.append(int(input("Enter score: ")))              #taking input from user and appending values in the list [append() does not require defining the array during declaration time, it'll keep on adding the values in the list each time it is invoked]
 average=str(sum(scores)/len(scores))
-print(f"Average of all scores is: {average}")
+print(f"Average of all scores is: {average}")               
+#similarly, for outputting multiple such lines, fstrings can be used as
+#eg - print(f"Your salary gross: {x}"+f"\nSalary after social contribution: {y}"+f"\nNet salary after tax: {z}") (Different lines printed)
