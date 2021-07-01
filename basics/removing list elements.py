3f3e2fb1b71a046df97c458f976612733ecf406e
Khushi Mishra <khushimishra26peace@gmail.com>
2021-07-01 10:28:29 +0530

    Create removing list elements.py

---
+++ basics/removing list elements.py
@@ -0,0 +1,5 @@
+# A list defined to show deletion of element (by value and by index)
+water_level=[123,124,125,126,127,128]
+water_level.remove('124')             #removes the element with value 124, i.e. second element of list
+#OR the same can be done by
+del water_level[1]                    #in Python, lists of size n begin with index 0.. and go till n-1
